d5f0d0ebf2c8aac832de4354736b1a3a69810a04
Bhoomi  Sapke <sapkebhoomi28@gmail.com>
2026-09-14 21:54:22 +0530

    GO to HOme

--- frontend/src/pages/student/Dashboard.jsx
+++ frontend/src/pages/student/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 import EventCard from "../../components/EventCard";
 import {
   CalendarDays,
@@ -51,7 +51,7 @@ import "./Dashboard.css";
 // ];
 
 const Dashboard = () => {
-  
+  const navigate = useNavigate();
   const API_URL = "http://127.0.0.1:8000";
 
   const [events, setEvents] = useState([]);
@@ -76,7 +76,7 @@ const Dashboard = () => {
             "Unable to load events."
           );
         }
-
+        
         const eventList = Array.isArray(data)
           ? data
           : data.results || [];
@@ -96,17 +96,24 @@ const Dashboard = () => {
 
   return (
     <div className="student-page">
-
+      <div className="dashboard-home-action">
+      <button
+        type="button"
+        className="go-home-btn"
+        onClick={() => navigate("/")}
+      >
+        Go to Home
+      </button>
+    </div>
       {/* ================= HERO ================= */}
       <section className="student-hero">
 
         <div className="student-hero-content">
-
-          <div className="student-badge">
+    
+           <div className="student-badge">
             <span></span>
             STUDENT
           </div>
-
           <h1>
             Discover the
             <br />
